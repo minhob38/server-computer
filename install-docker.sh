e8fdc5a298759e1adc35b32adbf3575795f201ec
kwon, minho <minhob38@gmail.com>
2021-12-25 00:31:11 +0900

    add ssh, docker

--- install-docker.sh
+++ install-docker.sh
@@ -13,9 +13,11 @@ sudo apt-get install \
   gnupg \
   lsb-release
 
-$ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
 
-$ echo \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
