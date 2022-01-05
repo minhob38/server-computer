1560104f985ea7e64dc366775badb60c44130113
kwon, minho <minhob38@gmail.com>
2022-01-05 22:39:25 +0900

    fix jenkins dockerfile

--- dockers/jenkins/install_docker.sh
+++ dockers/jenkins/install_docker.sh
@@ -27,8 +27,3 @@ echo "=== docker compose install ==="
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
-
-docker version
-docker-compose --version
-
-echo "end - install docker and docker compose"
