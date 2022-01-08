21f6e66f34f1dcf56616a1ec55cc7b722daae4c7
kwon, minho <minhob38@gmail.com>
2022-01-08 18:17:17 +0900

    set log

--- backend/deploy_backend.sh
+++ backend/deploy_backend.sh
@@ -1,7 +1,22 @@
 CURDIR=`dirname $0`
 
+docker-compose -f $CURDIR/docker-compose.backend.yml pull
+
+docker-compose -p server_a -f $CURDIR/docker-compose.backend.yml down
+echo "server a down"
+sleep 5
+
 docker-compose -p server_a -f $CURDIR/docker-compose.backend.yml up -d
+echo "server a up"
+
+sleep 1
+
+docker-compose -p server_b -f $CURDIR/docker-compose.backend.yml down
+echo "server b down"
 
 sleep 15
 
 docker-compose -p server_b -f $CURDIR/docker-compose.backend.yml up -d
+echo "server b up"
+
+# 무중단 배포를 확인하기 위해서, 서버를 하나 down 하고 up 하지 않기
