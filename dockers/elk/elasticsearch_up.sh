015bcb7ca2f20e99587778a16e275fd018d7fc8b
kwon, minho <minhob38@gmail.com>
2022-03-30 00:17:41 +0900

    baseline for normal state

--- dockers/elk/elasticsearch_up.sh
+++ dockers/elk/elasticsearch_up.sh
@@ -3,3 +3,26 @@ echo "=== elasticsearch up ==="
 CURDIR=`dirname $0`
 sudo docker-compose -f $CURDIR/docker-compose.elk.yml up -d
 
+
+#     http.host: "0.0.0.0"
+# xpack.monitoring.elasticsearch.hosts: [ "http://elasticsearch:9200" ]
+
+# ## X-Pack security credentials
+# xpack.monitoring.enabled: true
+# xpack.monitoring.elasticsearch.username: elastic
+# xpack.monitoring.elasticsearch.password: changeme!!!
+# pip line
+# input {
+#   tcp {
+#     port => 5000
+#     codec => json
+#   }
+# }
+
+# output {
+# 	elasticsearch {
+# 		hosts => "elasticsearch:9200"
+# 		user => "elastic"
+# 		password => "changeme!!!"
+# 	}
+# }
